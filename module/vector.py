bfe71f6c7827ab2051fbbbaf48d879bfbd87b281
tamco61 <42232243+tamco61@users.noreply.github.com>
2022-11-09 20:09:56 +0300

    Color and Vector
    add new classes

---
+++ module/vector.py
@@ -0,0 +1,13 @@
+class Vector:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __len__(self):
+        return (self.x ** 2 + self.y ** 2) ** 0.5
