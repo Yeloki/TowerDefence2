10a042ff7147296e27739add1e049de32e4c461b
slava <slava111103@gmail.com>
2022-11-19 00:14:45 +0300

    START

--- GeometryObject/GeometryFunc.py
+++ GeometryObject/GeometryFunc.py
@@ -1,17 +1,20 @@
 import pygame as pg
 
 
-def draw():
-    pass
+class GeometryObject:
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
 
 
-def update():
-    pg.display.update()
+class Circle(GeometryObject):
+    def __init__(self, depth, radius, color, coords):
+        self.__depth = depth
+        self.__radius = radius
+        self.__color = color
+        self.__coords = coords
 
-
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
+    def draw(self, display):
+        pg.draw.circle(display, self.__color, self.__radius, self.__depth)
