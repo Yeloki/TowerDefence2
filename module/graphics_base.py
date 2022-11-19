4cc3c540b62c544a07e5a4756705ef2226dd1c86
Yeloki <48127028+Yeloki@users.noreply.github.com>
2022-11-19 14:04:05 +0300

    Update graphics_base.py

--- module/graphics_base.py
+++ module/graphics_base.py
@@ -3,16 +3,16 @@ class Color:
         self.rgba = (r, g, b, a)
 
 
-class Vector:
+class Vector2:
     def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
     def __add__(self, other):
-        return Vector(self.x + other.x, self.y + other.y)
+        return Vector2(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other):
-        return Vector(self.x - other.x, self.y - other.y)
+        return Vector2(self.x - other.x, self.y - other.y)
 
     def __len__(self):
         return (self.x ** 2 + self.y ** 2) ** 0.5
