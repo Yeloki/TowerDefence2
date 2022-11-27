f14b573deaedfb25490759b621a02ccf534b18c0
slava <slava111103@gmail.com>
2022-11-27 11:24:44 +0300

    Mod

--- src/base/geometry.py
+++ src/base/geometry.py
@@ -1,3 +1,6 @@
+from ..graphics.graphics_base import Color
+
+
 class Vector2:
     def __init__(self, x: float, y: float):
         self.x = x
@@ -17,3 +20,19 @@ class Rect:
     def __init__(self, left_up: Vector2, size: Vector2):
         self.left_up_corner: Vector2 = left_up
         self.size: Vector2 = size
+
+
+class Circle:
+    def __init__(self, depth, radius, color: Color, coords):
+        self.__depth = depth
+        self.__radius = radius
+        self.__color = color
+        self.__coords = coords
+
+
+class Line:
+    def __init__(self, color: Color, start, end, width: int):
+        self.__color = color
+        self.__start_pos = start
+        self.__end_pos = end
+        self.__width = width
--- src/graphics/objects.py
+++ src/graphics/objects.py
@@ -1,17 +1,1 @@
-import graphics_base as graph
 
-
-class Circle:
-    def __init__(self, depth, radius, color: graph.Color, coords):
-        self.__depth = depth
-        self.__radius = radius
-        self.__color = color
-        self.__coords = coords
-
-
-class Line:
-    def __init__(self, color: graph.Color, start, end, width: int):
-        self.__color = color
-        self.__start_pos = start
-        self.__end_pos = end
-        self.__width = width
