44c0939c4be4b44b84bd46b8b4925254ffc9fc5b
slava <slava111103@gmail.com>
2022-11-27 11:03:47 +0300

    Graphics added

---
+++ src/graphics/objects.py
@@ -0,0 +1,6 @@
+class Circle():
+    def __init__(self, depth, radius, color, coords):
+        self.__depth = depth
+        self.__radius = radius
+        self.__color = color
+        self.__coords = coords
