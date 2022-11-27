9ec1e8d329e5b0199882d1aaef35b5c4188e09a0
slava <slava111103@gmail.com>
2022-11-27 11:21:18 +0300

    wip

--- src/graphics/objects.py
+++ src/graphics/objects.py
@@ -1,6 +1,17 @@
-class Circle():
-    def __init__(self, depth, radius, color, coords):
+import graphics_base as graph
+
+
+class Circle:
+    def __init__(self, depth, radius, color: graph.Color, coords):
         self.__depth = depth
         self.__radius = radius
         self.__color = color
         self.__coords = coords
+
+
+class Line:
+    def __init__(self, color: graph.Color, start, end, width: int):
+        self.__color = color
+        self.__start_pos = start
+        self.__end_pos = end
+        self.__width = width
