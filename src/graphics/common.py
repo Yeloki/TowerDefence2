505b7d434287f24442095288f219c51b480ea969
yeloki <gtgtr3000@gmail.com>
2022-11-27 18:04:06 +0300

    fix window and graphics

--- src/graphics/common.py
+++ src/graphics/common.py
@@ -1,3 +1,3 @@
 class Color:
-    def __init__(self, r: int, g: int, b: int, a: float):
+    def __init__(self, r: int, g: int, b: int, a: int = 255):
         self.rgba = (r, g, b, a)
--- src/graphics/window.py
+++ src/graphics/window.py
@@ -1,14 +1,18 @@
 import pygame
 from base import Vector2
 from tools import generate_uid
+from base import Color
 from tools.settings import SETTINGS
 
 
 class Window:
     def __init__(self):
         self.__size = Vector2(SETTINGS['window']['width'], SETTINGS['window']['height'])
-        self.__surf = pygame.display.set_mode(tuple(self.__size), pygame.SHOWN | pygame.DOUBLEBUF | pygame.OPENGL)
+        self.__surf = pygame.display.set_mode(tuple(self.__size), pygame.DOUBLEBUF)
         self.__uid = generate_uid()
+
+    def fill(self, color: Color):
+        self.__surf.fill(color.rgba)
 
     def display(self):
         pygame.display.flip()
