f5e63a21525e614894e8ea96e286e0d9aaed304e
tamco61 <42232243+tamco61@users.noreply.github.com>
2022-11-27 12:05:18 +0300

    Update objects.py

--- src/graphics/objects.py
+++ src/graphics/objects.py
@@ -1,1 +1,18 @@
+from ..base import Circle, Rect, Line
+import pygame
+
+
+class DrawableCircle(Circle):
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.__color.rgba, self.__center, self.__radius)
+
+
+class DrawableRect(Rect):
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.__color.rgba, self.__center, self.__radius)
+
+
+class DrawableLine(Line):
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.__color.rgba, self.__center, self.__radius)
 
